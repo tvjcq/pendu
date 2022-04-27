ba1a3ad8f00546ba49f515c1eeb3378925c56698
tvjcq <varsovie.trading@gmail.com>
2022-04-27 20:19:13 +0200

    add assets + update interface

--- interface.py
+++ interface.py
@@ -1,1 +1,21 @@
+import tkinter
 from tkinter import *
+
+
+window = Tk()
+
+# Personalisation fenêtre
+window.title("Le pendu de bobby")
+window.geometry("720x480")
+window.minsize(480, 360)
+window.iconbitmap("./assets/logo.ico")
+window.config(background="#404040")
+
+##################################
+
+label_title = Label(window, text="Bienvenue sur le pendu de bobby", font=("Arial", 25), bg="#404040", fg="white")
+label_title.pack()
+
+
+window.mainloop()
+
