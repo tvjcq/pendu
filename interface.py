3475db75839bf5434742db0afc80ec7aee378ae9
tvjcq <varsovie.trading@gmail.com>
2022-04-28 16:22:44 +0200

    update interface

--- interface.py
+++ interface.py
@@ -1,6 +1,5 @@
-import tkinter
 from tkinter import *
-
+# from main import *
 
 window = Tk()
 
@@ -12,10 +11,26 @@ window.iconbitmap("./assets/logo.ico")
 window.config(background="#404040")
 
 ##################################
+frame_title = Frame(window, bg="red", bd=1)
+frame_subtitle = Frame(window, bg="black", bd=1)
+frame_button = Frame(window, bg="white")
 
-label_title = Label(window, text="Bienvenue sur le pendu de bobby", font=("Arial", 25), bg="#404040", fg="white")
+gt_button = Button(window, text="Le github de bobby", font=("Arial", 12), bg="white", fg="black")
+gt_button.pack(pady=10, side=TOP)
+frame_button.pack(side=TOP)
+
+label_title = Label(frame_title, text="Bienvenue sur le pendu de bobby", font=("Arial", 25), bg="#404040", fg="white")
 label_title.pack()
+
+label_subtitle = Label(frame_subtitle, text="Entrez votre lettre", font=("Arial", 20), bg="#404040", fg="white")
+label_subtitle.pack()
+frame_title.pack(side=TOP)
+frame_subtitle.pack(side=TOP)
+
+
+
+
+
 
 
 window.mainloop()
-
